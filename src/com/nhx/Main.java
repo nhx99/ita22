c971fb5a2545a85d301ad6f12884a00ee4f1e178
Nedim <razmisli@gmail.com>
2022-02-04 16:05:21 +0100

    BubbleSort Vjezba 02-02-2022

--- src/com/nhx/Main.java
+++ src/com/nhx/Main.java
@@ -1,12 +1,28 @@
 package com.nhx;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] numbers = {19,5,9,55,6,3,1,70};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Koliko brojeva želite da unesete?");
+        int size = scanner.nextInt();
+        System.out.println("Unesite " + size + " brojeva:");
+
+        int count = 0;
+        int[] numbers = new int[size];
+
+        while(count < size) {
+            int number = scanner.nextInt();
+            numbers[count] = number;
+            count++;
+        }
+
            for (int i = 1; i < numbers.length; i++) {
 
                for (int j = 0; j < numbers.length; j++) {
